f649c3ee351ae0fe63b7f6f66047564ec9e5daf3
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-05-11 21:48:40 +0200

    [master] select cell setting

--- Bridge game/Bridge game/ViewController.swift
+++ Bridge game/Bridge game/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    let cells = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]
+    let cells = ["1", "2", "3", "4", "5",
+                 "6", "7", "8", "9", "10",
+                 "11", "12", "13", "14", "15",
+                 "16", "17", "18", "19", "20",
+                 "21", "22", "23", "24", "25"]
+    
+    var gridSituation = [0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         
     }
 
@@ -24,17 +33,24 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        
         cell.cellLabel.text = cells[indexPath.item]
+        if(gridSituation[indexPath.item] == 1) {
+            cell.backV.backgroundColor = UIColor.blue
+        }
+        else {
+            cell.backV.backgroundColor = UIColor.gray
+        }
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-        cell.changeColor(newColor: UIColor.blue)
         
-        print(indexPath.item + 1)
+        gridSituation[indexPath.item] = 1
+        collectionView.reloadData()
+        
+        print("pressed on \(indexPath.item + 1)")
+        print(gridSituation)
     }
 }
 
